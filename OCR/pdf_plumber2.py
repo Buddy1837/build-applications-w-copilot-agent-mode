7b406fc705b0df61560971646942cd1176287534
Buddy1837 <chetansutariya5324@gmail.com>
2025-05-02 14:28:21 +0530

    Delete OCR/pdf_plumber2.py

--- OCR/pdf_plumber2.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("sample of interview skills.pdf") as pdf:
-    for i, page in enumerate(pdf.pages):
-        text = page.extract_text()
-        print(f" ----------------------------------- Page {i+1} -----------------------------------\n{text}")
-
